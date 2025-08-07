e702fac8b9403250c60621102176195b904f4822
root <dhkim95@hotmail.com>
2025-08-07 07:44:01 +0000

    add getLoanRate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import log4js from "log4js";
 
 import getStockInfo from "./api/get-stock-info.js";
+import getLoanRate from "./api/get-loan-rate.js";
 
 log4js.configure("./config/log4js.json");
 
@@ -13,6 +14,7 @@ app.use(log4js.connectLogger(log4js.getLogger("http"), { level: "auto" }));
 app.use(express.json());
 
 app.get("/api/get-stock-info", getStockInfo);
+app.get("/api/get-loan-rate", getLoanRate);
 
 app.listen(port, () => {
   logger.info(`Example app listening on port ${port}`);
