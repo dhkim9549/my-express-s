7e5055c71f5831af9866d806a3f8c33b96d8bcf2
root <dhkim95@hotmail.com>
2025-08-07 03:20:16 +0000

    add stock info web scraping

--- api/get-stock-info.js
+++ api/get-stock-info.js
@@ -4,6 +4,7 @@
  */
 
 import log4js from "log4js";
+import { tabletojson } from "tabletojson";
 
 const logger = log4js.getLogger("app");
 
@@ -13,12 +14,31 @@ export default async function getStockInfo(req, res) {
 
   logger.info("req.query = " + JSON.stringify(req.query));
 
-  let resData = [];
-  let a = {};
-  a.stockNm = "하하하"
-  resData.push(a);
+  let resData = await main(req.query.stockNm);
 
   res.json(resData);
 
   logger.info("getStockInfo() end...");
+}
+
+let apiUrl = "https://api.scraperapi.com/?api_key=a6feef9544ee61c2d5b078836ca638f4&url=https%3A%2F%2Ffinance.naver.com%2Fsise%2Fsise_market_sum.naver";
+
+async function main(query) {
+
+  logger.info("main() start...");
+  logger.info("query = " + query);
+
+  let table = "";
+  await tabletojson.convertUrl(apiUrl, function (tablesAsJson) {
+    table = tablesAsJson[1];
+  });
+
+  let table2 = table.filter(function (e) {
+    return e?.종목명?.startsWith(query);
+  });
+
+  logger.info("table2 = " + table2);
+  logger.info("main() end...");
+
+  return table2;
 }
